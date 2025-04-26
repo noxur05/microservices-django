15bf4f4a68087b72cf2bf60ad11eee28c6f7ed83
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-04-26 12:46:42 +0500

    added object.all to queryset

--- products/viewsets.py
+++ products/viewsets.py
@@ -2,5 +2,5 @@ from rest_framework import viewsets
 from .serializers import ProductSerializer
 from .models import Product
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
